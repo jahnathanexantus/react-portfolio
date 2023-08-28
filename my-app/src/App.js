7cfcfb12cba76b1ddb858fa3892e5a30bea074ea
jahnathan x <jahnathanexantus@gmail.com>
2023-08-27 23:07:21 -0400

    scrapping initial starting on new one, finished file structure

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import "./App.css";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
+import Portfolio from "./pages/portfolio/Portfolio";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="portfolio" element={<Portfolio />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
