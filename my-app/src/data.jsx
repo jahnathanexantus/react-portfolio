36388437df4b9d3f409b90d5ba10003092be6259
jahnathan x <jahnathanexantus@gmail.com>
2023-09-01 13:34:25 -0400

    completed the resumeitem file and the accompanying css

--- my-app/src/data.jsx
+++ my-app/src/data.jsx
@@ -1,148 +1,193 @@
 import About from './pages/about/About'
 
 import {
-    FaHome,
-    FaUser,
-    FaFolderOpen,
-    FaEnvelopeOpen,
-    FaBriefcase,
-    FaGraduationCap,
-    FaCode
-}  from 'react-icons/fa'
+  FaHome,
+  FaUser,
+  FaFolderOpen,
+  FaEnvelopeOpen,
+  FaBriefcase,
+  FaGraduationCap,
+  FaCode
+} from 'react-icons/fa'
 
-
-
-export const links =[
-    {
-        id:1,
-        name:'Home',
-        icon: <FaHome className='nav__icon'/>,
-        path:'/'
-    },
-    {
-        id:2,
-        name:'About',
-        icon: <FaUser className='nav__icon'/>,
-        path:'/about'
-    },
-    {
-        id:3,
-        name:'Portfolio',
-        icon: <FaFolderOpen className='nav__icon'/>,
-        path:'/portfolio'
-    },
-    {
-        id:4,
-        name:'Contact',
-        icon: <FaEnvelopeOpen className='nav__icon'/>,
-        path:'/contact'
-    },
+export const links = [
+  {
+    id: 1,
+    name: 'Home',
+    icon: <FaHome className='nav__icon' />,
+    path: '/'
+  },
+  {
+    id: 2,
+    name: 'About',
+    icon: <FaUser className='nav__icon' />,
+    path: '/about'
+  },
+  {
+    id: 3,
+    name: 'Portfolio',
+    icon: <FaFolderOpen className='nav__icon' />,
+    path: '/portfolio'
+  },
+  {
+    id: 4,
+    name: 'Contact',
+    icon: <FaEnvelopeOpen className='nav__icon' />,
+    path: '/contact'
+  }
 ]
-
 
 export const personalInfo = [
-    {
-        id:1,
-        name: ' First Name :',
-        description: 'Jahnathan'
-    },
-    {
-        id:2,
-        name: ' Last Name :',
-        description: 'Exantus'
-    },
-    {
-        id:3,
-        name: ' age :',
-        description: ' 39 Years'
-    },
-    {
-        id:4,
-        name: ' Nationality :',
-        description: ' Haitian'
-    },
-    {
-        id:5,
-        name: ' Address :',
-        description: ' 8171 Cloverglen Circle Olando, Fl 32818'
-    },
-    {
-        id:6,
-        name: ' Email :',
-        description: ' jahnathanexantus@gmail.com'
-    },
-    {
-        id:7,
-        name: ' Languages :',
-        description: ' English, Creole ,Spanish'
-    }
+  {
+    id: 1,
+    name: ' First Name :',
+    description: 'Jahnathan'
+  },
+  {
+    id: 2,
+    name: ' Last Name :',
+    description: 'Exantus'
+  },
+  {
+    id: 3,
+    name: ' age :',
+    description: ' 39 Years'
+  },
+  {
+    id: 4,
+    name: ' Nationality :',
+    description: ' Haitian'
+  },
+  {
+    id: 5,
+    name: ' Address :',
+    description: ' 8171 Cloverglen Circle Olando, Fl 32818'
+  },
+  {
+    id: 6,
+    name: ' Email :',
+    description: ' jahnathanexantus@gmail.com'
+  },
+  {
+    id: 7,
+    name: ' Languages :',
+    description: ' English, Creole ,Spanish'
+  }
 ]
 
-
 export const stats = [
-    {
-        id:1,
-        no: ' 1-1/2 + :',
-        title: ' Years of <br/> Experience'
-    },
-    {
-        id:2,
-        no: ' 5+ :',
-        title: ' Completed <br/> Projects'
-    },
-    {
-        id:2,
-        no: ' 5+ :',
-        title: ' Completed <br/> Projects'
-    },
-    {
-        id:2,
-        no: ' 5+ :',
-        title: ' Completed <br/> Projects'
-    },
-    
+  {
+    id: 1,
+    no: ' 1-1/2 + :',
+    title: ' Years of <br/> Experience'
+  },
+  {
+    id: 2,
+    no: ' 5+ :',
+    title: ' Completed <br/> Projects'
+  },
+  {
+    id: 2,
+    no: ' 5+ :',
+    title: ' Completed <br/> Projects'
+  },
+  {
+    id: 2,
+    no: ' 5+ :',
+    title: ' Completed <br/> Projects'
+  }
 ]
 
 export const skills = [
-    {
-        id:1,
-        title:'html',
-        percentage: '25'
-    },
-    {
-        id:1,
-        title:'html',
-        percentage: '25'
-    },
-    {
-        id:1,
-        title:'html',
-        percentage: '25'
-    },
-    {
-        id:1,
-        title:'html',
-        percentage: '25'
-    },
-    {
-        id:1,
-        title:'html',
-        percentage: '25'
-    },
-    {
-        id:1,
-        title:'html',
-        percentage: '25'
-    },
-    {
-        id:1,
-        title:'html',
-        percentage: '25'
-    },
-    {
-        id:1,
-        title:'html',
-        percentage: '25'
-    }
+  {
+    id: 1,
+    title: 'html',
+    percentage: '25'
+  },
+  {
+    id: 1,
+    title: 'html',
+    percentage: '25'
+  },
+  {
+    id: 1,
+    title: 'html',
+    percentage: '25'
+  },
+  {
+    id: 1,
+    title: 'html',
+    percentage: '25'
+  },
+  {
+    id: 1,
+    title: 'html',
+    percentage: '25'
+  },
+  {
+    id: 1,
+    title: 'html',
+    percentage: '25'
+  },
+  {
+    id: 1,
+    title: 'html',
+    percentage: '25'
+  },
+  {
+    id: 1,
+    title: 'html',
+    percentage: '25'
+  }
 ]
 
+export const resume = [
+  {
+    id: 1,
+    category: 'experience',
+    icon: <FaBriefcase />,
+    year: '2018 -present',
+    title: 'Full-stack <span>Envato</span>',
+    desc: 'lorem ipsum kajg;kenf ;akjdgh;akejhf dafjgadfjgka slkgjs;kjg kjg'
+  },
+  {
+    id: 2,
+    category: 'experience',
+    icon: <FaBriefcase />,
+    year: '2018 -present',
+    title: 'Full-stack <span>Envato</span>',
+    desc: 'lorem ipsum kajg;kenf ;akjdgh;akejhf dafjgadfjgka slkgjs;kjg kjg'
+  },
+  {
+    id: 3,
+    category: 'experience',
+    icon: <FaBriefcase />,
+    year: '2018 -present',
+    title: 'Full-stack <span>Envato</span>',
+    desc: 'lorem ipsum kajg;kenf ;akjdgh;akejhf dafjgadfjgka slkgjs;kjg kjg'
+  },
+  {
+    id: 4,
+    category: 'education',
+    icon: <FaBriefcase />,
+    year: '2018 -present',
+    title: 'Full-stack <span>Envato</span>',
+    desc: 'lorem ipsum kajg;kenf ;akjdgh;akejhf dafjgadfjgka slkgjs;kjg kjg'
+  },
+  {
+    id: 5,
+    category: 'education',
+    icon: <FaBriefcase />,
+    year: '2018 -present',
+    title: 'Full-stack <span>Envato</span>',
+    desc: 'lorem ipsum kajg;kenf ;akjdgh;akejhf dafjgadfjgka slkgjs;kjg kjg'
+  },
+  {
+    id: 6,
+    category: 'education',
+    icon: <FaBriefcase />,
+    year: '2018 -present',
+    title: 'Full-stack <span>Envato</span>',
+    desc: 'lorem ipsum kajg;kenf ;akjdgh;akejhf dafjgadfjgka slkgjs;kjg kjg'
+  }
+]
--- my-app/src/pages/about/About.jsx
+++ my-app/src/pages/about/About.jsx
@@ -4,6 +4,8 @@ import Stats from '../../components/Stats'
 import { FaDownload } from 'react-icons/fa'
 import './about.css'
 import Skills from '../../components/Skills'
+import { resume } from '../../data'
+import ResumeItem from '../../components/ResumeItem'
 // import CV from '../../assets'
 
 const About = () => {
@@ -41,6 +43,32 @@ const About = () => {
 
         <div className="skills__container grid">
           <Skills/>
+        </div>
+      </section>
+
+      <div className="separator"></div>
+
+      <section className="resume">
+      <h3 className="section__subtitle  subtitle__center">
+        Experience & Education
+        </h3>
+
+        <div className="resume__container grid">
+          <div className="resume__data">
+            {resume.map((val)=>{
+              if (val.category === 'experience'){
+                return <ResumeItem key={val.id} {...val}/>
+              }
+            })}
+          </div>
+
+          <div className="resume__data">
+            {resume.map((val)=>{
+              if (val.category === 'education'){
+                return <ResumeItem key={val.id} {...val}/>
+              }
+            })}
+          </div>
         </div>
       </section>
     </main>
